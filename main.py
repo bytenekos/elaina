fc82aa76dd69785ecaaf6bab68af487396ae6bee
_Sukuratchi <66184829+Sukuratchi@users.noreply.github.com>
2024-03-09 04:11:28 +0300

    Add logger with folder creation + config file stuff

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import asyncio
 import discord
 import os
+import logging
 from discord.ext import commands
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
-loadedCogs = []
 MY_GUILD = discord.Object(id=1116469018019233812)
+loadedCogs = []
+log_folder = 'logs'
+os.makedirs(log_folder, exist_ok=True)
+log_file_name = f"log_date_{datetime.now().strftime('%Y-%m-%d')}_time_{datetime.now().strftime('%H-%M-%S')}.txt"
+log_file_path = os.path.join(log_folder, log_file_name)
+
+logger = logging.getLogger('__name__')
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s:%(levelname)s:%(filename)s: %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p',
+                    filename=log_file_path,)
 
 intents = discord.Intents.default()
 intents.members = True
@@ -30,6 +42,7 @@ async def loadCogs():
         if file.endswith('.py'):
             loadedCogs.append(file[:-3])
             await bot.load_extension(f'cogs.{file[:-3]}')
+            logging.info(f'Loaded cog {file[:-3]}')
 
 
 async def main():
