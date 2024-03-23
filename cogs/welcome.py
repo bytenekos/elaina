ee6e2df083eaf9a16509e018e84ce9ed6681ec08
_Sukuratchi <66184829+Sukuratchi@users.noreply.github.com>
2024-03-23 17:08:14 +0300

    Remove random welcome messages and role giving
    This functionality has been moved to role.py cog, only purpose for this cog for now is to track when a new user joins and log that into log

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -20,17 +20,6 @@ class Welcome(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member):
         logger.info(f'Member joined: {member}')
-        responses = [
-            f"Lets all welcome in <@{member.id}>!",
-            f"Hey look, <@{member.id}> has joined the server!",
-            f"Hi there <@{member.id}>! Hope you enjoy your stay here.",
-            f"Welcome to the server, <@{member.id}>!",
-        ]
-        myguild = self.bot.get_guild(1116469018019233812)
-        welcome = myguild.get_channel(1178850385901932564)
-        role = myguild.get_role(1179086276935299164)
-        await member.add_roles(role)
-        await welcome.send(random.choice(responses))
 
 
 async def setup(bot):
