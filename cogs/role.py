1293845a66b65d6e4380b85cd7ca8fbaa1ef7e5b
_Sukuratchi <66184829+Sukuratchi@users.noreply.github.com>
2024-03-23 17:11:30 +0300

    Adds a sendverify command with persistent button
    This adds a persistent button, which after a user presses, gives them a role as well as send a welcome message in a welcome channel!

--- cogs/role.py
+++ cogs/role.py
@@ -1,5 +1,6 @@
 import discord
 import logging
+import random
 from discord import app_commands
 from discord.ext import commands
 
@@ -15,8 +16,17 @@ class TestButton(discord.ui.View):
 
     @discord.ui.button(label="Accept", style=discord.ButtonStyle.green, custom_id="accept")
     async def accept(self, interaction: discord.Interaction, Button: discord.ui.Button):
-        await interaction.user.add_roles(discord.utils.get(interaction.guild.roles, id=1179400425510809711))
-        await interaction.response.send_message(content="works", ephemeral=True)
+        logger.info(f'Member accepted: {interaction.user.name}, ID: {interaction.user.id}')
+        responses = [
+            f"Lets all welcome in <@{interaction.user.id}>!",
+            f"Hey look, <@{interaction.user.id}> has joined the server!",
+            f"Hi there <@{interaction.user.id}>! Hope you enjoy your stay here.",
+            f"Welcome to the server, <@{interaction.user.id}>!",
+        ]
+        welcomechannel = discord.utils.get(interaction.guild.channels, id=1178850385901932564)
+        await interaction.user.add_roles(discord.utils.get(interaction.guild.roles, id=1179086276935299164))
+        await welcomechannel.send(random.choice(responses))
+        await interaction.response.send_message(content="You've been accepted, enjoy the server!", ephemeral=True)
 
 
 class Role(commands.Cog):
@@ -27,9 +37,17 @@ class Role(commands.Cog):
     async def on_ready(self):
         logger.info(f'Roles cog loaded!')
 
-    @app_commands.command(name="testaccept", description="test")
-    async def testaccept(self, interaction: discord.Interaction):
-        await interaction.response.send_message(content="accepted", view=TestButton())
+    @app_commands.command(name="sendverify", description="sends a verification message to a channel")
+    @app_commands.checks.has_role(1120840113170157599)
+    async def accept(self, interaction: discord.Interaction):
+        await interaction.channel.send(content="Press the button below if you've read and accept the rules!", view=TestButton())
+        await interaction.response.send_message(content="Sent button!", ephemeral=True)
+
+    @accept.error
+    async def on_sync_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError):
+        if isinstance(error, app_commands.MissingRole):
+            await interaction.response.send_message(f"Verify command failed: {error}", ephemeral=True)
+            logger.error(f"Verify command failed: {error}")
 
 
 async def setup(bot):
