c659b92aed50d2f8a5ac3f34850cf964fde5a481
_Sukuratchi <66184829+Sukuratchi@users.noreply.github.com>
2024-03-23 16:21:32 +0300

    Refactor role cog and make button persistent
    Holy fuck I can't believe I figured this out

--- cogs/role.py
+++ cogs/role.py
@@ -9,6 +9,16 @@ logging.basicConfig(level=logging.INFO,
                     datefmt='%m/%d/%Y %I:%M:%S %p')
 
 
+class TestButton(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+
+    @discord.ui.button(label="Accept", style=discord.ButtonStyle.green, custom_id="accept")
+    async def accept(self, interaction: discord.Interaction, Button: discord.ui.Button):
+        await interaction.user.add_roles(discord.utils.get(interaction.guild.roles, id=1179400425510809711))
+        await interaction.response.send_message(content="works", ephemeral=True)
+
+
 class Role(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
@@ -17,19 +27,11 @@ class Role(commands.Cog):
     async def on_ready(self):
         logger.info(f'Roles cog loaded!')
 
-    class TestButton(discord.ui.View):
-        def __init__(self):
-            super().__init__(timeout=None)
-
-        @discord.ui.button(label="Accept", style=discord.ButtonStyle.green, custom_id="accept")
-        async def accept(self, interaction: discord.Interaction, Button: discord.ui.Button):
-            await interaction.user.add_roles(discord.utils.get(interaction.guild.roles, id=1179400425510809711))
-            await interaction.response.send_message(content="works", ephemeral=True)
-
     @app_commands.command(name="testaccept", description="test")
     async def testaccept(self, interaction: discord.Interaction):
-        await interaction.response.send_message(content="accepted", view=self.TestButton())
+        await interaction.response.send_message(content="accepted", view=TestButton())
 
 
 async def setup(bot):
+    bot.add_view(TestButton())
     await bot.add_cog(Role(bot), guilds=[discord.Object(id=1116469018019233812)])
