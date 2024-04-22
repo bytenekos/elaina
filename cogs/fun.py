b7d32df8b5ebef9b70dae28bb6eaf634656f91fd
_Sukuratchi <66184829+Sukuratchi@users.noreply.github.com>
2024-04-22 06:16:46 +0300

    Remove unused variable

--- cogs/fun.py
+++ cogs/fun.py
@@ -12,7 +12,6 @@ logging.basicConfig(level=logging.INFO,
 
 
 def ballchoice():
-    ballrandom = random.randint(1, 20)
     randomchoice = ["it is certain.",
                     "it is decidedly so.",
                     "without a doubt.",
