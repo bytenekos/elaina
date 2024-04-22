0120c26e795072238a6101d6e0213a5cbfa503ca
_Sukuratchi <66184829+Sukuratchi@users.noreply.github.com>
2024-04-22 23:22:09 +0300

    Add parsing and download functions

--- utils/util7tv.py
+++ utils/util7tv.py
@@ -1,4 +1,8 @@
 import aiohttp
+import aiofiles
+import asyncio
+import re
+import os
 
 
 async def get7tvEmoteList(link: str):
@@ -9,3 +13,36 @@ async def get7tvEmoteList(link: str):
                 return await resp.json()
             else:
                 return None
+
+
+async def parseGuildEmotes(guildEmotes: f""):
+    animated_emotes = []
+    non_animated_emotes = []
+    pattern = r"name='(.*?)'\s+animated=(.*?)\s"
+    matches = re.findall(pattern, guildEmotes)
+    for name, animated_str in matches:
+        animated = animated_str.strip() == "True"
+        if animated:
+            animated_emotes.append(name)
+        else:
+            non_animated_emotes.append(name)
+
+    return len(animated_emotes), len(non_animated_emotes)
+
+
+async def download7tvEmote(emoteid: str, emotename: str):
+    url = f"https://cdn.7tv.app/emote/{emoteid}/4x.webp"
+    file_name = f"{emotename}.webp"
+    folder_path = "temp"
+
+    os.makedirs(folder_path, exist_ok=True)
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                file_path = os.path.join(folder_path, file_name)
+                async with aiofiles.open(file_path, mode="wb") as f:
+                    await f.write(await resp.read())
+                    print(f"Downloaded {url} to {file_path}")
+            else:
+                print(f"Failed to download {url}, status code: {resp.status}")
